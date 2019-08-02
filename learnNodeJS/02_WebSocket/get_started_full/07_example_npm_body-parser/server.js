14469965cfe90e5033b4eb658437c16cfb62844d
ilinoa <ilinoa90@gmail.com>
2019-08-02 18:13:23 +0300

    add post req and create page for view req

--- learnNodeJS/02_WebSocket/get_started_full/07_example_npm_body-parser/server.js
+++ learnNodeJS/02_WebSocket/get_started_full/07_example_npm_body-parser/server.js
@@ -8,38 +8,27 @@ app.set('view engine', 'ejs');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use('/', urlencodedParser , function (require, respose) {
-	if(require.url == '/' || require.url == '/index'){
-		console.log(require.url);
-		respose.render('index', {});
-	}
-	else if (require.url == '/about') {
-		console.log(require.url);
-		respose.render('about', {});
-	}
-	else {
-		console.log(require.url);
-		respose.render('404', {});
-	}
+
+app.get('/index', function (req, res) {
+		console.log(req.url);
+		res.render('index', {});
 });
 
+app.post('/index', urlencodedParser, function (req, res) {
+	if (!req.body) return res.sendStatus(400);
+	console.log(req.body);
+	res.render('index-success', {data: req.body});
+});
 
-
-// app.get('/index', function (require, respose) {
-// 		console.log(require.url);
-// 		respose.render('index', {});
-// 	});
-// app.post('/index', urlencodedParser, function (require, respose) {
-// 	if (!require.body) {return respose.sendStatus(400);}
-// 	console.log(require.url);
-// 	respose.render('index-success', {});
-// });
-
-// app.get('/about', function (require, respose) {
-// 		console.log(require.url);
-// 		respose.render('about', {});
-// });
+app.get('/about', function (req, res) {
+		console.log(req.url);
+		res.render('about', {});
+});
 
 
 app.listen(3000);
 console.log('Server run...');
+
+
+
+
