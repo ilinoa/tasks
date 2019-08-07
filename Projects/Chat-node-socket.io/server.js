08877e9c5d9cb9f1855161600430e8d6150a4762
ilinoa <ilinoa90@gmail.com>
2019-08-07 18:36:43 +0300

    add ejs and js script

--- Projects/Chat-node-socket.io/server.js
+++ Projects/Chat-node-socket.io/server.js
@@ -1,16 +1,43 @@
 var express = require('express');
-var app = require();
+var app = express();
 var server = require('http').createServer(app);
-var io = require('server.io');
+var io = require('socket.io').listen(server);
 
-server.listen(3000);
+var bodyParser = require('body-parser');
+app.use('/public', express.static('public'))
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + 'index.html');
+app.set('view engine', 'ejs', 'js')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+server.listen(3000)
+
+app.get('/', function (req, res) {
+		console.log(req.url)
+		res.render('start', {})
+})
+
+users = [] // хранит всех пользователей с которыми есть соединение
+connections = [] // помещяем сюда все подключения на текущий момент
+
+
+/*
+*		io.sockets.on - отслеживает 'connection' и вызывает  function, 
+*		которая содержит объект socket. 
+* 		.push(socket) - добавление в массив connections
+*		.splice(connections.indexOf(socket), 1) - на ходит индекс 
+*		объекта и удаляет 1 элемент
+*/		 
+io.sockets.on('connection', function(socket) {
+	console.log('connect new user')
+	connections.push(socket)
+
+	socket.on('disconnect', function(data){
+		console.log('user disconnected')
+		connections.splice(connections.indexOf(socket), 1)
+	})
 })
 
 
 
-
-
-
+console.log('Server run, port 3000')
