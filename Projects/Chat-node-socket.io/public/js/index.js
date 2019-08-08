5c5fd11c80bfd7e1380cf55509d2a17f71aa9f6c
ilinoa <ilinoa90@gmail.com>
2019-08-08 18:24:25 +0300

    add message field and time

--- Projects/Chat-node-socket.io/public/js/index.js
+++ Projects/Chat-node-socket.io/public/js/index.js
@@ -1,1 +1,36 @@
-alert("Hello world")
+
+$(function(){
+	var socket = io.connect() 			// подключаем socket -> connection
+	var $form = $('#messForm') 			// обращаемся к объекту по Id - messForm
+	var $time = $('time')
+	var $input_mess = $('#message') 	// поле ввода
+	var $all_mess = $('#all_mess')		// поле вывода
+	
+	now = new Date();
+	setTimeout(function(){
+		 window.location.reload(1);
+   	
+	}, 60000);
+	
+	/*
+	*	отслеживаем в форме событие submit
+	*	preventDefault() - предотвращает стандартное поведение
+	*	socket обращается к функции emit() для вызова события - укажет 'send mess' и данные поле ввода
+	*	после отправки сообщения указываем путую строку
+	*/
+	$form.submit(function(event){
+		event.preventDefault()
+		console.log(now)
+		socket.emit('send mess',  {mess:$input_mess.val()})
+		$input_mess.val('')
+	})
+
+	/*
+	*	когда вызовется 'add mess' -  обратиться к функции emit()
+	*  	append() - встраивает блок внутрь all_mess
+	* 	
+	*/
+	socket.on('add mess', function(data){
+		$all_mess.append(now+'<div>' + data.mess + '</div>')
+	})
+})
--- Projects/Chat-node-socket.io/server.js
+++ Projects/Chat-node-socket.io/server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
 
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 app.use('/public', express.static('public'))
 
 app.set('view engine', 'ejs', 'js')
@@ -24,20 +24,32 @@ connections = [] // помещяем сюда все подключения на
 /*
 *		io.sockets.on - отслеживает 'connection' и вызывает  function, 
 *		которая содержит объект socket. 
-* 		.push(socket) - добавление в массив connections
-*		.splice(connections.indexOf(socket), 1) - на ходит индекс 
-*		объекта и удаляет 1 элемент
 */		 
 io.sockets.on('connection', function(socket) {
+
+	/*
+	* 	.push(socket) - добавление в массив connections
+	*	.splice(connections.indexOf(socket), 1) - на ходит индекс 
+	*	объекта и удаляет 1 элемент
+	*/
 	console.log('connect new user')
 	connections.push(socket)
-
 	socket.on('disconnect', function(data){
 		console.log('user disconnected')
 		connections.splice(connections.indexOf(socket), 1)
 	})
+
+	/*
+	*	когда вызовется 'send mess' -  обратиться к функции emit()
+	*  	'add mess' - передаст mess со значением data  
+	*/
+	socket.on('send mess', function(data){
+		io.sockets.emit('add mess', {mess: data.mess})
+	})
+
+
 })
 
-
+	
 
 console.log('Server run, port 3000')
