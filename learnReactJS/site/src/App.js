f980a33c7f9bf899efeee56783210205951972eb
ilinoa <ilinoa90@gmail.com>
2019-08-16 23:31:48 +0300

    add weather api_key

--- learnReactJS/site/src/App.js
+++ learnReactJS/site/src/App.js
@@ -5,15 +5,15 @@ import Weather from "./components/weather"
 
 const API_KEY = "b3aeea4d5a7aa6196ff809fc4242fa34";
 
-
 class App extends React.Component {
-  
-gettingWeather = async () => {
-  const api_url = await 
-  fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`)
-  const data = await api_url.json()
-  console.log()
-}
+
+  gettingWeather = async (event) => {
+    event.preventDefault()
+    const api_url = await 
+    fetch(`samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`)
+    var data = await api_url.json()
+    console.log(data)
+  }
 
 
   render() {
