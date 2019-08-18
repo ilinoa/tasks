7327a457532d795f00341493eaded6c693ae11ab
ilinoa <ilinoa90@gmail.com>
2019-08-18 23:56:28 +0300

    bug fixed with json

--- learnReactJS/site/src/App.js
+++ learnReactJS/site/src/App.js
@@ -3,15 +3,14 @@ import Info from "./components/info"
 import Form from "./components/form"
 import Weather from "./components/weather"
 
-const API_KEY = "b3aeea4d5a7aa6196ff809fc4242fa34";
+const APIKEY = "b3aeea4d5a7aa6196ff809fc4242fa34"
 
 class App extends React.Component {
 
   gettingWeather = async (event) => {
     event.preventDefault()
-    const api_url = await 
-    fetch(`samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`)
-    var data = await api_url.json()
+    const api_url = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${APIKEY}`)
+    const data = api_url.json()
     console.log(data)
   }
 
